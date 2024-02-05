afe4bb7be4eba0a2a84e8bd63728132e127c8e75
SafakB <safakbahce@gmail.com>
2024-02-05 12:03:22 +0300

    add comments

--- backup.ps1
+++ backup.ps1
@@ -1,28 +1,28 @@
-# Geçerli tarih ve saati formatlayarak yedek adını oluştur
+# Create the backup name by formatting the current date and time
 $backupName = "backup_" + (Get-Date -Format "yyyyMMdd_HHmm")
 
-# Backup klasörünü oluştur
+# Create the Backup folder
 New-Item -ItemType Directory -Force -Path .\$backupName
 
-# MariaDB veritabanı yedeğini al
+# Backup MariaDB database
 docker-compose exec mariadb sh -c 'exec mysqldump --all-databases --add-drop-database -u"$MYSQL_USER" -p"$MYSQL_PASSWORD"' > ".\${backupName}\dump.sql"
 
-# Appwrite hacimlerini yedekle
+# Backup Appwrite volumes
 $appwrite_volumes = @("uploads", "cache", "config", "certificates", "functions")
 foreach ($volume in $appwrite_volumes) {
     docker run --rm --volumes-from $(docker compose ps -q appwrite) -v "${PWD}\${backupName}:/backup" ubuntu bash -c "cd /storage/$volume && tar cvf /backup/$volume.tar ."
 }
 
-# Appwrite_appwrite-mariadb hacmini yedekle
+# Backup appwrite_appwrite-mariadb volume
 docker run --rm --volume appwrite_appwrite-mariadb:/var/lib/mysql --volume "${PWD}\${backupName}:/backup" ubuntu tar -cvf /backup/mariadb.tar -C /var/lib/mysql .
 
-# appwrite_appwrite-influxdb hacmini yedekle
+# Backup appwrite_appwrite-influxdb volume
 docker run --rm --volume appwrite_appwrite-influxdb:/var/lib/docker/volumes/appwrite_appwrite-influxdb/_data --volume "${PWD}\${backupName}:/backup" ubuntu tar -cvf /backup/influxdb.tar -C /var/lib/docker/volumes/appwrite_appwrite-influxdb/_data .
 
-# Appwrite-worker-deletes hacmini yedekle
+# Backup appwrite-worker-deletes volume
 docker run --rm --volumes-from $(docker compose ps -q appwrite-worker-deletes) -v "${PWD}\${backupName}:/backup" ubuntu bash -c "cd /storage/builds && tar cvf /backup/builds.tar ."
 
-# Konfigürasyon dosyalarını kopyala
+# Copy configuration files
 Copy-Item docker-compose.yml -Destination ".\${backupName}"
 Copy-Item .env -Destination ".\${backupName}"
 if (Test-Path docker-compose.override.yml) {
